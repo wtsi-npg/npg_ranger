14fbcab837cde6a880a43109d44d815c74060b8d
Marina Gourtovaia <mg8@sanger.ac.uk>
2016-04-12 16:22:37 +0100

    pipeline module and tests; grunt task for generating docs

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,23 +3,74 @@ module.exports = function(grunt) {
   require( 'load-grunt-tasks' )( grunt );
 
   grunt.initConfig({
+
+    jsdoc: {
+      src: ['lib/*.js'],
+      options: {
+        destination: 'doc'
+      }
+    },
+
     jscs: {
-      main: [ 'Gruntfile.js',
-              'bin/server.js',
+      main: [ 'bin/server.js',
+              'lib/*.js'
       ],
       options: {
         config: '.jscsrc'
       }
     },
+
     jshint: {
       all: [
         'Gruntfile.js',
-        'bin/server.js'
+        'bin/server.js',
+        'lib/*.js'
       ],
       options: {
         jshintrc: '.jshintrc'
       }
     },
+
+    jasmine_nodejs: {
+      // task specific (default) options 
+      options: {
+        specNameSuffix: "spec.js", // also accepts an array
+        helperNameSuffix: "helper.js",
+        useHelpers: false,
+        random: false,
+        seed: null,
+        //defaultTimeout: null, // defaults to 5000 
+        defaultTimeout: 5000, 
+        stopOnFailure: false,
+        traceFatal: 2,
+        // configure one or more built-in reporters 
+        reporters: {
+          console: {
+            colors: true,        // (0|false)|(1|true)|2 
+            cleanStack: 0,       // (0|false)|(1|true)|2|3 
+            verbosity: 4,        // (0|false)|1|2|3|(4|true) 
+            listStyle: "indent", // "flat"|"indent" 
+            activity: false
+          },
+        },
+        // add custom Jasmine reporter(s) 
+        customReporters: []
+      },
+      'server_tests': {
+        // target specific options 
+        //options: {
+        //    useHelpers: true
+        //},
+        // spec files 
+        specs: [
+          "test/**"
+        ]
+        //helpers: [
+        //    "test/helpers/**"
+        //]
+      }
+    },
+
     watch: {
       js: {
         files: [
@@ -36,6 +87,6 @@ module.exports = function(grunt) {
   });
 
   grunt.registerTask('lint', ['jshint', 'jscs']);
-  grunt.registerTask('test', ['lint']);
+  grunt.registerTask('test', ['lint', 'jasmine_nodejs']);
   grunt.registerTask('default', ['test']);
 };
---
+++ test/pipeline.spec.js
@@ -0,0 +1,166 @@
+"use strict";
+
+const spawn    = require('child_process').spawn;
+const devnull  = require('dev-null');
+const pipeline = require('../lib/pipeline.js');
+
+var path = 'test/data/pipeline/text.txt';
+var isSuccess = null;
+
+var setup = function(done, cat_command, f, wc_command, wc_options) {
+
+  const cat = spawn(cat_command, [f]);
+  cat.title = 'cat';
+  var prs = [cat];
+  
+  if (wc_command) {
+    if (! wc_options) {
+      wc_options = '-l';
+    }
+    const wc  = spawn(wc_command, [wc_options]);
+    wc.title = 'wc';
+    prs.push(wc);
+  }
+
+  // The following two functions will be called asynchronously
+  isSuccess = null;
+  var success = function() {isSuccess = true;  done()};
+  var failure = function() {isSuccess = false; done()};
+
+  var pline = pipeline(prs, success, failure);
+  pline.run(devnull());
+}
+
+describe('Run function input validation', function() {
+
+  const cat = spawn('cat', [path]);
+  var fun = function() {};
+ 
+  it('Destination is not given - error', function() {
+    expect( () => {pipeline([cat], fun, fun).run()} ).toThrowError(ReferenceError,
+    'Destination stream is not defined');
+  });
+
+  
+  it('Array of processes should be defined', function() {
+    expect( () => {pipeline().run(devnull())} ).toThrowError(
+      ReferenceError, 'Array of processes should be defined'
+    );
+  });
+  it('Array of processes cannot be null', function() {
+    expect( () => {pipeline(null, fun, fun).run(devnull())} ).toThrowError(
+      ReferenceError, 'Array of processes should be defined'
+    );
+  });
+  it('Processes should be an array', function() {
+    expect( () => {pipeline(fun, fun).run(devnull())} ).toThrowError(
+      TypeError, 'processes should be an array'
+    );
+  });
+  it('Processes array cannot be empty', function() {
+    expect( () => {pipeline([], fun, fun).run(devnull())} ).toThrowError(
+      RangeError, 'processes array cannot be empty'
+    );
+  });
+  it('All process array members have to be defined', function() {
+    expect( () => {pipeline([cat, null, cat], fun, fun).run(devnull())} ).toThrowError(
+      ReferenceError, 'Undefined process at index 1'
+    );
+  });
+  it('Process shoudl be an instance of EventEmitter', function() {
+    expect( () => {pipeline([cat, 'dog', cat], fun, fun).run(devnull())} ).toThrowError(
+      TypeError, 'Not an event emitter at index 1'
+    );
+  });
+
+  it('Success callback should be defined', function() {
+    expect( () => {pipeline([cat]).run(devnull())} ).toThrowError(
+      ReferenceError, 'Success callback should be defined'
+    );
+  });
+  it('Success callback should be a function', function() {
+    expect( () => {pipeline([cat], 'callback').run(devnull())} ).toThrowError(
+      TypeError, 'Success callback should be a function'
+    );
+  });
+  it('Failure callback should be defined', function() {
+    expect( () => {pipeline([cat], fun).run(devnull())} ).toThrowError(
+      ReferenceError, 'Failure callback should be defined'
+    );
+  });
+  it('Failure callback should be a function', function() {
+    expect( () => {pipeline([cat], fun, 'callback').run(devnull())} ).toThrowError(
+      TypeError, 'Failure callback should be a function'
+    );
+  });  
+});
+
+describe('Valid two-process pipeline', function() {
+  beforeEach(function(done) {
+    setup(done, 'cat', path, 'wc');
+  });
+  it('Pipeline succeeds', function(done) {
+    expect(isSuccess).toBe(true);
+    done();
+  });    
+});
+
+describe('Valid one-process pipeline', function() {
+  beforeEach(function(done) {
+    setup(done, 'cat', path);
+  });
+  it('Pipeline succeeds', function(done) {
+    expect(isSuccess).toBe(true);
+    done();
+  });    
+});
+
+describe('Valid two-process pipeline, invalid input', function() {
+  beforeEach(function(done) {
+    setup(done, 'cat', 'test/data/pipeline/test.txt', 'wc');
+  });
+  it('Pipeline fails', function(done) {
+    expect(isSuccess).toBe(false);
+    done();
+  });    
+});
+
+describe('Valid one-process pipeline, invalid input', function() {
+  beforeEach(function(done) {
+    setup(done, 'cat', 'test/data/pipeline/test.txt');
+  });
+  it('Pipeline fails', function(done) {
+    expect(isSuccess).toBe(false);
+    done();
+  });    
+});
+
+describe('Invalid pipeline, first command incorrect', function() {
+  beforeEach(function(done) {
+    setup(done, 'notcat', path, 'wc');
+  });
+  it('Pipeline fails', function(done) {
+    expect(isSuccess).toBe(false);
+    done();
+  });    
+});
+
+describe('Invalid pipeline, second command incorrect', function() {
+  beforeEach(function(done) {
+    setup(done, 'cat', path, 'nowc');
+  });
+  it('Pipeline fails', function(done) {
+    expect(isSuccess).toBe(false);
+    done();
+  });    
+});
+
+describe('Invalid pipeline, second command option is invalid', function() {
+  beforeEach(function(done) {
+    setup(done, 'cat', path, 'wc', '-something');
+  });
+  it('Pipeline fails', function(done) {
+    expect(isSuccess).toBe(false);
+    done();
+  });
+});
