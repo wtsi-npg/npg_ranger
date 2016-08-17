c5bc29ad88d5c2365ee6e5baac74a15bc1c031f5
Andrew Nowak <an8@sanger.ac.uk>
2016-08-17 14:07:49 +0100

    make strict mode default

--- bin/server.js
+++ bin/server.js
@@ -19,7 +19,7 @@ var opt = new GetOpt([
     ['m','mongourl=URI'     ,'URI to contact mongodb'],
     ['t','tempdir=PATH'     ,'PATH of temporary directory'],
     ['H','hostname=HOST'    ,'override hostname with HOST'],
-    ['S','strictmode'       ,'run in strict mode'],
+    ['' ,'no-strict'        ,'disable strict mode'],
     ['s','skipauth'         ,'skip authorisation steps'],
     ['d','debug'            ,'debugging mode for this server'],
     ['h','help'             ,'display this help']
@@ -129,9 +129,11 @@ MongoClient.connect(MONGO, MONGO_OPTIONS, function(err, db) {
 
     // Create an instance of an application controller and let it
     // handle the request.
+    // Invert no-strict to pass 'strict' to RangerController and
+    // avoid double negatives.
     let controller = new RangerController(
       request, response, db, TEMP_DATA_DIR, opt.options.skipauth,
-      opt.options.strict);
+      opt.options['no-strict']);
     controller.handleRequest(HOST);
   });
 
