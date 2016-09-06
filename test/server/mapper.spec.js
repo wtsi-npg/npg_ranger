d3a996b10e29e9a1b4eceaff88f5a11cd0cad2bc
jmtcsngr <jmtc@sanger.ac.uk>
2016-09-06 11:19:09 +0100

    fix indentation

--- test/server/mapper.spec.js
+++ test/server/mapper.spec.js
@@ -160,12 +160,13 @@ describe('Data info retrieval', function() {
       assert.equal(err, null);
       var dm = new DataMapper(db);
       dm.on('data', (data) => {
-        expect(data).toEqual(
-          [{file: 'irods:/seq/10000/10000_2#22.bam', accessGroup: '2574',
-            reference: '/Homo_sapiens/1000Genomes_hs37d5/all/fasta/hs37d5.fa'}]);
+        expect(data).toEqual([{
+          file: 'irods:/seq/10000/10000_2#22.bam', accessGroup: '2574',
+          reference: '/Homo_sapiens/1000Genomes_hs37d5/all/fasta/hs37d5.fa'
+        }]);
         done();
       });
-	    dm.getFileInfo({name: "10000_2#22.bam", directory: "/seq/10000"}, 'localhost');
+      dm.getFileInfo({name: "10000_2#22.bam", directory: "/seq/10000"}, 'localhost');
     });
   });
 
@@ -188,14 +189,20 @@ describe('Data info retrieval', function() {
       assert.equal(err, null);
       var dm = new DataMapper(db);
       dm.on('data', (data) => {
-       let d = [
-          {file: 'irods:/seq/10000/10000_4#43.bam', accessGroup: '2586',
-           reference: '/Homo_sapiens/1000Genomes_hs37d5/all/fasta/hs37d5.fa'},
-          {file: 'irods:/seq/10000/10000_5#74.bam', accessGroup: '',
-           reference: '/Caenorhabditis_elegans/101019/all/fasta/C_elegans_101019.fasta'},
-          {file: 'irods:/seq/10000/10000_7#92.bam', accessGroup: '0',
-           reference: '/Homo_sapiens/1000Genomes_hs37d5/all/fasta/hs37d5.fa'},
-		];
+        let d = [
+          {
+            file: 'irods:/seq/10000/10000_4#43.bam', accessGroup: '2586',
+            reference: '/Homo_sapiens/1000Genomes_hs37d5/all/fasta/hs37d5.fa'
+          },
+          {
+            file: 'irods:/seq/10000/10000_5#74.bam', accessGroup: '',
+            reference: '/Caenorhabditis_elegans/101019/all/fasta/C_elegans_101019.fasta'
+          },
+          {
+            file: 'irods:/seq/10000/10000_7#92.bam', accessGroup: '0',
+            reference: '/Homo_sapiens/1000Genomes_hs37d5/all/fasta/hs37d5.fa'
+          },
+        ];
         data.sort(compareFiles);
         expect(data).toEqual(d);
         done();
@@ -214,7 +221,7 @@ describe('Data info retrieval', function() {
           {file: '/irods-seq-sr04-ddn-gc10-30-31-32/seq/10000/10000_5#74.bam', accessGroup: '', reference: '/Caenorhabditis_elegans/101019/all/fasta/C_elegans_101019.fasta'},
           {file: '/irods-seq-sr04-ddn-gc10-30-31-32/seq/10000/10000_7#92.bam', accessGroup: '0', reference: '/Homo_sapiens/1000Genomes_hs37d5/all/fasta/hs37d5.fa'},
           {file: '/irods-seq-sr04-ddn-gc10-30-31-32/seq/10000/10000_8#97.bam', accessGroup: '2574', reference: '/Homo_sapiens/1000Genomes_hs37d5/all/fasta/hs37d5.fa'}
-		];
+        ];
         data.sort(compareFiles);
         expect(data).toEqual(d);
         done();
@@ -235,7 +242,7 @@ describe('Data info retrieval', function() {
            reference: '/Caenorhabditis_elegans/101019/all/fasta/C_elegans_101019.fasta'},
           {file: '/irods-seq-i10-bc/seq/10000/10000_7#92.bam', accessGroup: '0',
            reference: '/Homo_sapiens/1000Genomes_hs37d5/all/fasta/hs37d5.fa'},
-		    ];
+        ];
         d.sort(compareFiles);
         data.sort(compareFiles);
         expect(data).toEqual(d);
