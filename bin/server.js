34823741fbd82be76b58e251f84d60680aa6b3b8
jmtcsngr <jmtc@sanger.ac.uk>
2016-09-14 12:40:37 +0100

    split behaviour for main/require module

--- bin/server.js
+++ bin/server.js
@@ -11,155 +11,148 @@ const MongoClient = require('mongodb').MongoClient;
 const LOGGER      = require('../lib/logsetup.js');
 
 const config = require('../lib/config.js');
-// Call to config.provide() must occur here before requiring controller
-// so that options object is built before it is provided to the
-// other modules.
-const options = config.provide(config.fromCommandLine);
 
-const RangerController = require('../lib/server/controller');
+if ( require.main === module ) {
+  // Call to config.provide() must occur here before requiring controller
+  // so that options object is built before it is provided to the
+  // other modules.
+  const options = config.provide(config.fromCommandLine);
 
-const numWorkers = options.get('numworkers');
+  const RangerController = require('../lib/server/controller');
 
-if ( options.get('debug') ) {
-  LOGGER.level = 'debug';
-}
+  const numWorkers = options.get('numworkers');
 
-/*
- * Main server script. Create the server object, establish database,
- * connection, setup server callbacks, start listening for incoming
- * requests.
- *
- * Providing config settings:
- *  Settings are provided from 3 locations:
- *  1. Command line - run with -h to see options.
- *  2. Config json file can be read if it is provided on command line
- *      by running with -c PATH or --configfile=PATH
- *  3. There are some defaults, which can be found in lib/config.js
- */
-
-assert(process.env.USER, 'User environment variable is not defined');
-if ( cluster.isMaster ) {
-  LOGGER.info(config.logOpts());
-  for (let i = 0; i < numWorkers; i++) {
-    cluster.fork();
+  if ( options.get('debug') ) {
+    LOGGER.level = 'debug';
   }
-  let consec = 0;
-  cluster.on('exit', (worker, code, signal) => {
-    LOGGER.debug('Worker %d died (%s). Forking to replace ...', worker.id, signal || code);
-    let waitingConsec = options.get('clustertimeout');
-    let maxConsec = options.get('clustermaxdeaths');
-    LOGGER.debug(`${consec} forks have died in the previous ${waitingConsec} seconds.`);
-    if ( consec >= maxConsec ) {
-      LOGGER.error('Too many forks started in short span of time. Trying to exit now.');
-      process.exit(1);
+
+  /*
+   * Main server script. Create the server object, establish database,
+   * connection, setup server callbacks, start listening for incoming
+   * requests.
+   *
+   * Providing config settings:
+   *  Settings are provided from 3 locations:
+   *  1. Command line - run with -h to see options.
+   *  2. Config json file can be read if it is provided on command line
+   *      by running with -c PATH or --configfile=PATH
+   *  3. There are some defaults, which can be found in lib/config.js
+   */
+
+  assert(process.env.USER, 'User environment variable is not defined');
+  if ( cluster.isMaster ) {
+    LOGGER.info(config.logOpts());
+    for (let i = 0; i < numWorkers; i++) {
+      cluster.fork();
     }
-    consec += 1;
-    cluster.fork();
-    setTimeout( () => {
-      consec -= 1;
-    }, waitingConsec * 1000 );
-  });
-} else {
-  if ( cluster.isWorker ) {
-    LOGGER.debug('WORKER: new fork ' + cluster.worker.id);
-  }
-  const server = http.createServer();
+    let consec = 0;
+    cluster.on('exit', (worker, code, signal) => {
+      LOGGER.debug('Worker %d died (%s). Forking to replace ...', worker.id, signal || code);
+      let waitingConsec = options.get('clustertimeout');
+      let maxConsec = options.get('clustermaxdeaths');
+      LOGGER.debug(`${consec} forks have died in the previous ${waitingConsec} seconds.`);
+      if ( consec >= maxConsec ) {
+        LOGGER.error('Too many forks started in short span of time. Trying to exit now.');
+        process.exit(1);
+      }
+      consec += 1;
+      cluster.fork();
+      setTimeout( () => {
+        consec -= 1;
+      }, waitingConsec * 1000 );
+    });
+  } else {
+    if ( cluster.isWorker ) {
+      LOGGER.debug('WORKER: new fork ' + cluster.worker.id);
+    }
+    const server = http.createServer();
 
-  // Exit gracefully on a signal to quit
-  [ 'SIGTERM', 'SIGINT', 'SIGHUP' ].forEach( ( sig ) => {
-    process.on( sig, () => {
-      server.close( () => {
-        process.exit(0);
+    // Exit gracefully on a signal to quit
+    [ 'SIGTERM', 'SIGINT', 'SIGHUP' ].forEach( ( sig ) => {
+      process.on( sig, () => {
+        server.close( () => {
+          process.exit(0);
+        });
       });
     });
-  });
-  // Connect to the database and, if successful, define
-  // callbacks for the server.
-  var mongourl = options.get('mongourl');
-  MongoClient.connect(mongourl, options.get('mongoopt'), function(err, db) {
+    // Connect to the database and, if successful, define
+    // callbacks for the server.
+    var mongourl = options.get('mongourl');
+    MongoClient.connect(mongourl, options.get('mongoopt'), function(err, db) {
 
-    assert.equal(err, null, `Failed to connect to ${mongourl}: ${err}`);
-    LOGGER.info(`Connected to ${mongourl}`);
+      assert.equal(err, null, `Failed to connect to ${mongourl}: ${err}`);
+      LOGGER.info(`Connected to ${mongourl}`);
 
-    var dbClose = (dbConn) => {
-      if (dbConn) {
-        LOGGER.info('Database connection closing');
+      var dbClose = (dbConn) => {
+        if (dbConn) {
+          LOGGER.info('Database connection closing');
+          try {
+            dbConn.close();
+          } catch (err) {
+            LOGGER.error(`Error closing db connection: ${err}`);
+          }
+        }
+      };
+
+      // Close database connection on server closing.
+      server.on('close', () => {
+        LOGGER.info("\nServer closing");
+        dbClose(db);
+      });
+
+      // Exit gracefully on error, close the database
+      // connection and remove the socket file.
+      process.on('uncaughtException', (err) => {
+        LOGGER.error(`Caught exception: ${err}\n`);
+        dbClose(db);
         try {
-          dbConn.close();
+          let port = options.get('port');
+          if (typeof port != 'number') {
+            // Throws an error if the assertion fails
+            fs.accessSync(port, fs.W_OK);
+            LOGGER.info(`Remove socket file ${port} that is left behind`);
+            fs.unlinkSync(port);
+          }
         } catch (err) {
-          LOGGER.error(`Error closing db connection: ${err}`);
+          LOGGER.error(`Error removing socket file: ${err}`);
         }
-      }
-    };
-
-    // Close database connection on server closing.
-    server.on('close', () => {
-      LOGGER.info("\nServer closing");
-      dbClose(db);
-    });
-
-    // Exit gracefully on error, close the database
-    // connection and remove the socket file.
-    process.on('uncaughtException', (err) => {
-      LOGGER.error(`Caught exception: ${err}\n`);
-      dbClose(db);
-      try {
-        let port = options.get('port');
-        if (typeof port != 'number') {
-          // Throws an error if the assertion fails
-          fs.accessSync(port, fs.W_OK);
-          LOGGER.info(`Remove socket file ${port} that is left behind`);
-          fs.unlinkSync(port);
-        }
-      } catch (err) {
-        LOGGER.error(`Error removing socket file: ${err}`);
-      }
-      let code = 1;
-      LOGGER.info(`Exiting with code ${code}`);
-      process.exit(code);
-    });
-
-    // Set up a callback for requests.
-    server.on('request', (request, response) => {
-      LOGGER.debug("MEMORY USAGE: " + util.inspect(process.memoryUsage()) + "\n");
-      if (cluster.isWorker) {
-        LOGGER.debug("WORKER: served by " + cluster.worker.id);
-      }
-
-      // Ensure the processes initiated by request stops if the client disconnects.
-      // Closing the response forces an error in the pipeline and allows for a
-      // prompt closing of a socket established for this request.
-      request.on('close', () => {
-        LOGGER.info('CLIENT DISCONNECTED');
-        response.end();
+        let code = 1;
+        LOGGER.info(`Exiting with code ${code}`);
+        process.exit(code);
       });
 
-      // Create an instance of an application controller and let it
-      // handle the request.
-      LOGGER.debug('request headers: ' + JSON.stringify(request.headers));
-      let controller = new RangerController(request, response, db);
-      controller.handleRequest(options.get('hostname'));
+      // Set up a callback for requests.
+      server.on('request', (request, response) => {
+        LOGGER.debug("MEMORY USAGE: " + util.inspect(process.memoryUsage()) + "\n");
+        if (cluster.isWorker) {
+          LOGGER.debug("WORKER: served by " + cluster.worker.id);
+        }
+
+        // Ensure the processes initiated by request stops if the client disconnects.
+        // Closing the response forces an error in the pipeline and allows for a
+        // prompt closing of a socket established for this request.
+        request.on('close', () => {
+          LOGGER.info('CLIENT DISCONNECTED');
+          response.end();
+        });
+
+        // Create an instance of an application controller and let it
+        // handle the request.
+        let controller = new RangerController(request, response, db);
+        controller.handleRequest(options.get('hostname'));
+      });
+
+      var createTempDataDir = () => {
+        let tmpDir = options.get('tempdir');
+        LOGGER.debug(`Using temp data directory ${tmpDir}`);
+        fs.ensureDirSync(tmpDir);
+      };
+
+      // Synchronously create directory for temporary data, then start listening.
+      createTempDataDir(options.get('tempdir'));
+      server.listen(options.get('port'), () => {
+        LOGGER.info(`Server listening on ${options.get('hostname')}, ${options.get('port')}`);
+      });
     });
-
-    var createTempDataDir = () => {
-      let tmpDir = options.get('tempdir');
-      /*
-      if (!fs.existsSync(tmpDir)) {
-        fs.mkdirSync(tmpDir);
-        LOGGER.debug(`Created temp data directory ${tmpDir}`);
-      } else {
-        LOGGER.debug(`Found temp data directory ${tmpDir}`);
-      }
-      */
-      LOGGER.debug(`Using temp data directory ${tmpDir}`);
-      fs.ensureDirSync(tmpDir);
-
-    };
-
-    // Synchronously create directory for temporary data, then start listening.
-    createTempDataDir(options.get('tempdir'));
-    server.listen(options.get('port'), () => {
-      LOGGER.info(`Server listening on ${options.get('hostname')}, ${options.get('port')}`);
-    });
-  });
+  }
 }
