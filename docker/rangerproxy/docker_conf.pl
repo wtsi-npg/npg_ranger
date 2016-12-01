ba9abfdd23f5ff8e6faf3ae59b85f71b0c0cd7e9
jmtcsngr <jmtc@sanger.ac.uk>
2016-12-01 10:06:19 +0000

    use httpd.conf from 1.0.0 minus ssl

--- docker/rangerproxy/docker_conf.pl
+++ docker/rangerproxy/docker_conf.pl
@@ -6,11 +6,18 @@ use warnings;
 local @ARGV = ('httpd.conf');
 local $^I = '.bak';
 while (<>) {
-    s{CUSTOM_PORT  80$}{CUSTOM_PORT  9090};
+    s{CUSTOM_PORT[\s]+80$}{CUSTOM_PORT  9090};
     s{^LoadModule auth_openidc}{\#LoadModule auth_openidc};
     s{^\#User(.*)$}{User www-data};
     s{^\#Group(.*)$}{Group www-data};
     s{^OIDC}{\#OIDC};
+
+    s{^\#ServerName www.example.com:80}{ServerName localhost};
+
+    s{^LoadModule ssl_module}{\#LoadModule ssl_module};
+    s{^LoadModule proxy_connect_module}{\#LoadModule proxy_connect_module};
+    s{^SSL}{\#SSL};
+
     s{^Define RSOCKET_PATH(.*)$}{Define NPG_DOCKER 1};
     next if (m{^<(.*?)Location(.*)}
           or m{^( *)<(.*?)Limit(.*)}
