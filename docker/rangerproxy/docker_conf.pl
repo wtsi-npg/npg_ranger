7a16fcd646f33b7244e7a981e6accb376e8b06c5
jmtcsngr <jmtc@sanger.ac.uk>
2016-12-01 13:36:30 +0000

    configure apache with oidc when module loaded

--- docker/rangerproxy/docker_conf.pl
+++ docker/rangerproxy/docker_conf.pl
@@ -10,13 +10,11 @@ while (<>) {
     s{^LoadModule auth_openidc}{\#LoadModule auth_openidc};
     s{^\#User(.*)$}{User www-data};
     s{^\#Group(.*)$}{Group www-data};
-    s{^OIDC}{\#OIDC};
 
     s{^\#ServerName www.example.com:80}{ServerName localhost};
 
     s{^LoadModule ssl_module}{\#LoadModule ssl_module};
     s{^LoadModule proxy_connect_module}{\#LoadModule proxy_connect_module};
-    s{^SSL}{\#SSL};
 
     s{^Define RSOCKET_PATH(.*)$}{Define NPG_DOCKER 1};
     next if (m{^<(.*?)Location(.*)}
